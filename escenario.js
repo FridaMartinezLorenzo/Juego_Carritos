478488050dbd814bb968849b5b6505d9b3756e2d
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-10-21 19:10:25 -0600

    Se añadio material grafico

---
+++ escenario.js
@@ -0,0 +1,7 @@
+// Aquí puedes agregar JavaScript para interactuar con el canvas
+var canvas = document.getElementById("miCanvas");
+var ctx = canvas.getContext("2d");
+
+// Ejemplo: Dibujar un rectángulo rojo en el canvas
+ctx.fillStyle = "red";
+ctx.fillRect(50, 50, 200, 100);
