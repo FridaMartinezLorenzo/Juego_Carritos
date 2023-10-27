8318bfcb3c5bc14551015b178b122b0aa833ddb8
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2023-10-27 06:20:14 -0600

    Se tiene que checar la parte de detectar choques

--- escenario.js
+++ escenario.js
@@ -12,7 +12,13 @@ var img_start = new Image();
 img_start.src = "assets/frente_yellow_car.png";
 
 var img_car_enemigo = new Image();
-img_car_enemigo.src = "assets/pink_car.png";
+img_car_enemigo.src = "assets/purple_car.png";
+var img_car_enemigo2 = new Image();
+img_car_enemigo2.src = "assets/pink_car.png";
+var img_car_enemigo3 = new Image();
+img_car_enemigo3.src = "assets/blue_car.png";
+var img_meta = new Image();
+img_meta.src = "assets/meta.jpg";
 
 var x = 0; //Aqui es donde va a empezar
 var y = y_pista+110;
@@ -29,14 +35,25 @@ margen_desaparicion_carrito = 140;
 intervalo_carrito = 30;
 
 var contador = 0;
+var puntaje = 0;
 
 //Variables para carrito enemigo
-var x_enemigo = canvas.width;
-var y_enemigo = y_pista + 10;
-var intervalo_carrito_enemigo = 40;
 var dx_carrito_enemigo = 2;
-car_ancho_enemigo = 120;
-car_altura_enemigo = 80;
+var car_ancho_enemigo = 120;
+var car_altura_enemigo = 80;
+var cars = [img_car_enemigo,img_car_enemigo2,img_car_enemigo3];
+
+//A lo mas saldrán tres carritos enemigos al mismo tiempo
+//Coordenadas iniciales del carrito enemigo
+var x_enemigo = 0;
+var y_enemigo = 0;
+//Coordenadas iniciales del carrito enemigo1 
+var x_enemigo1 = 0;
+var y_enemigo1 = 0;
+//Coordenadas iniciales del carrito enemigo2 
+var x_enemigo2 = 0;
+var y_enemigo2 = 0;
+
 
 //Variables del boton
 inicio_x_boton = canvas.width/2-100;
@@ -50,10 +67,17 @@ alto_boton = 50;
 //El carrito o premio que se va a dibujar, en que punto se va a dibujar (con respecto al contador) y su ubicacion x, y
 let L = [
     [   //primer nivel (0)
-        [{e:0},{e:0},{e:0},{e:0},{e:0},{e:2},{e:0},{e:0},{e:0}],
+        [{car:1, appear:100,  x:canvas.width, y:y_pista+10 , bandera_rebaso:0},
+         {car:2, appear:400,  x:canvas.width, y:y_pista+110, bandera_rebaso:0},
+         {car:2, appear:700,  x:canvas.width, y:y_pista+10 , bandera_rebaso:0},
+         {car:3, appear:1000, x:canvas.width, y:y_pista+110, bandera_rebaso:0}],
     ],
     [   //segundo nivel (1)
-        [{e:2},{e:3},{e:0},{e:1},{e:1},{e:3},{e:2},{e:0},{e:2}], 
+       [{car:2, appear:100,  x:canvas.width, y:y_pista+10 , bandera_rebaso:0},
+        {car:3, appear:400,  x:canvas.width, y:y_pista+110, bandera_rebaso:0},
+        {car:1, appear:700,  x:canvas.width, y:y_pista+10 , bandera_rebaso:0},
+        {car:3, appear:1000, x:canvas.width, y:y_pista+110, bandera_rebaso:0}],
+
         [{e:3},{e:1},{e:2},{e:0},{e:3},{e:0},{e:1},{e:2},{e:3}],
         [{e:2},{e:0},{e:1},{e:1},{e:2},{e:2},{e:0},{e:3},{e:3}],
     ],
@@ -65,28 +89,64 @@ let L = [
     ]
     ]
     
+var nivel = 0;
 
+//Variable para la aparicion de los carritos
+contador  = 0;
+contador_carritos = 0;
 
-//var nivel = 1;
-//Trabajamos en la animación de la pistas
-
-
-//for ( nivel = 1; nivel<=2 ;nivel++){
-//    dibujar_pista();
-//    ancho_pista += 60;
-//}
 
 
 img.onload = function (){ 
-
+}
+img_car_enemigo.onload = function (){ 
+}
+img_car_enemigo2.onload = function (){
+}
+img_car_enemigo3.onload = function (){
+}
+img_meta.onload = function (){
 }
 
 img_start.onload = function (){ //carga la imagen y despues ejecuta la funcion 
         cuadro_inicial();
-    // setInterval(draw,50);
 }
 document.addEventListener("keydown",detectarTecla);
-canvas.addEventListener("click", function(event) {
+canvas.addEventListener("click", detectarClick);
+
+
+function cuadro_inicial(){
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    ctx.fillStyle = "#FF69B4";
+    ctx.fillRect(canvas.width/2-200,canvas.height/2-100,400,200);
+    
+    ctx.fillStyle = "#FF97D9";
+    ctx.fillRect(canvas.width/2-210,canvas.height/2-106,400,200);
+
+    ctx.drawImage(img_start, canvas.width/2-150,10, 300, 200);
+
+    //Dibujamos el boton
+    ctx.fillStyle = "#FF69B4";
+    ctx.fillRect(inicio_x_boton,inicio_y_boton,200,50);
+
+    // Configura el estilo del texto
+    ctx.font = "20px Times New Roman";
+    ctx.fillStyle = "white";
+
+    // Centra el texto en el botón
+    var buttonText = "START";
+    var textWidth = ctx.measureText(buttonText).width;
+    var xText = (canvas.width / 2) - (textWidth / 2);
+    var yText = (canvas.height / 2 + 20) + 10;
+
+    // Dibuja el texto en el botón
+    ctx.fillText(buttonText, xText, yText);
+
+}
+
+function detectarClick(event){
     console.log("click");
     var rect = canvas.getBoundingClientRect();
     var x_click = event.clientX - rect.left;
@@ -98,10 +158,10 @@ canvas.addEventListener("click", function(event) {
 
     // Verifica si el clic está dentro del botón, INICIA EL JUEGOOOOOO
     if (x_click >= inicio_x_boton &&  x_click <= inicio_x_boton+ancho_boton && y_click >= inicio_y_boton  && y_click <= inicio_y_boton+alto_boton ) {
-        setInterval(draw,intervalo_carrito);
+        var IntervaloJuego = setInterval(draw,intervalo_carrito);
         //setInterval(dibujar_carrito_enemigo,intervalo_carrito_enemigo);
     }
-});
+}
 
 function detectarTecla(e){
     if (e.keyCode == 39){
@@ -136,49 +196,42 @@ function detectarTecla(e){
         x = canvas.width-margen_desaparicion_carrito;
 }
 
-function cuadro_inicial(){
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0,0,canvas.width,canvas.height);
-
-    ctx.fillStyle = "#FF69B4";
-    ctx.fillRect(canvas.width/2-200,canvas.height/2-100,400,200);
-    
-    ctx.fillStyle = "#FF97D9";
-    ctx.fillRect(canvas.width/2-210,canvas.height/2-106,400,200);
-
-    ctx.drawImage(img_start, canvas.width/2-150,10, 300, 200);
-
-    //Dibujamos el boton
-    ctx.fillStyle = "#FF69B4";
-    ctx.fillRect(inicio_x_boton,inicio_y_boton,200,50);
-
-    // Configura el estilo del texto
-    ctx.font = "20px Times New Roman";
-    ctx.fillStyle = "white";
-
-    // Centra el texto en el botón
-    var buttonText = "START";
-    var textWidth = ctx.measureText(buttonText).width;
-    var xText = (canvas.width / 2) - (textWidth / 2);
-    var yText = (canvas.height / 2 + 20) + 10;
-
-    // Dibuja el texto en el botón
-    ctx.fillText(buttonText, xText, yText);
-
-}
-
 function draw(){
     draw_escenario();
     dibujar_carrito();
+    detectar_rebaso();
+    detectar_colision_carrito_enemigo();
+
+
+    ctx.font="24px Arial"
+    ctx.fillStyle="black"
+    ctx.fillText("Score:"+puntaje,canvas.width-200,20);
+
     x += dx;
+    dibujar_carritos_enemigos();
     if (x > canvas.width-margen_desaparicion_carrito || x < 0)
-        dx = (-1)*dx;
+    dx = -dx;
 
+    if (y < y_pista || y>y_pista+ancho_pista){ 
+        dy = -dy;
+    }
     contador += 1;
-    if (contador > 20)
-        dibujar_carrito_enemigo();
-}
 
+    if (contador ==1){
+        contador_carritos = 0;
+
+        //Contamos el numero de carritos que apareceran en el nivel
+        for (j = 0; j < L[nivel].length; j++ ){
+            for (i = 0; i < L[nivel][j].length; i++)
+            {
+               contador_carritos += 1;
+            }
+        }
+        //console.log("contador_carritos: " + contador_carritos);
+
+    }
+
+}
 
 function draw_escenario(){
      //Rosa y morado #EE97E5
@@ -191,12 +244,120 @@ function dibujar_carrito(){
     ctx.drawImage(img, x, y, car_ancho, car_altura);
 }
 
-function dibujar_carrito_enemigo(){
-    ctx.drawImage(img_car_enemigo, x_enemigo, y_enemigo, car_ancho_enemigo, car_altura_enemigo);
-    x_enemigo -= dx_carrito_enemigo;
-    //if (x_enemigo < canvas.width/2)
-    //    dx_carrito_enemigo = (-1)*dx_carrito_enemigo;
+function dibujar_carritos_enemigos(){
+    for (j = 0; j < L[nivel].length; j++ ){
+        for (i = 0; i < L[nivel][j].length; i++)
+        {
+            c = L[nivel][j][i];
+            if(c.car != 0){
+                //Evluamos si es el momento en el que debe de aparecer el carrito
+                //console.log("contador: " + contador + " appear: " + c.appear);
+                if (contador >= c.appear){
+                    dibujar_carrito_enemigo(j,i);
+                }
+            }
+        }
+    }
+
 }
+
+function dibujar_carrito_enemigo(j,i){
+    enemigo = L[nivel][j][i];
+
+    ctx.drawImage(cars[enemigo.car - 1], enemigo.x, enemigo.y, car_ancho_enemigo, car_altura_enemigo);
+    // Actualiza la posición del carrito enemigo
+    enemigo.x -= dx_carrito_enemigo;
+}
+
+function detectar_colision_carrito_enemigo(){
+    var direccionColision = "";
+    for (j = 0; j < L[nivel].length; j++ ){
+        for (i = 0; i < L[nivel][j].length; i++)
+        {
+            var c = L[nivel][j][i];
+            if (c.x + car_ancho_enemigo > 0){ //Se encuentra en pantalla
+                if(x < c.x + car_ancho && x + car_ancho > c.x &&
+                    y < c.y + car_altura && y + car_altura > c.y){
+
+                         // Determina la dirección de la colisión
+                            if (y < c.y+car_altura && x < c.x+car_ancho) {
+                                direccionColision = "abajo";
+                            }
+                            if (y+car_altura > c.y && x+car_ancho < c.x) {
+                                direccionColision = "arriba";
+                            }
+                       
+                        // Realiza el ajuste de coordenadas para el rebote
+                        if (direccionColision == "arriba") {
+                            if (c.y >= y_pista + (ancho_pista/2) ) {
+                                //Si se encuentra por debajo de la mitad de la pista
+                                 y = c.y - car_altura;
+                            }
+                            else {
+                                y = c.y + car_altura;
+                            }
+                            x = c.x - car_ancho;
+                        } else if (direccionColision == "abajo") {
+                            if (c.y >= y_pista + (ancho_pista/2) ){
+                                //Si se encuentra por debajo de la mitad de la pista
+                                y = c.y - car_altura;
+                            } 
+                            else{
+                                y = c.y + car_altura;
+                            } 
+                            x = c.x - car_ancho;
+                        } 
+                           
+                }
+            }
+
+        }
+    }
+
+}
+
+function detectar_rebaso(){
+    var total = 1;
+    for (j = 0; j < L[nivel].length; j++ ){
+        for (i = 0; i < L[nivel][j].length; i++)
+        {
+            var c = L[nivel][j][i];
+            total *=c.bandera_rebaso;
+            if (x > c.x +car_ancho && c.bandera_rebaso == 0){ //Se encuentra en una posicion adelante del carro enemigo
+                puntaje  += c.car * 10;
+                c.bandera_rebaso = 1;
+                console.log("puntaje: " + puntaje);
+            }
+
+        }
+    }
+
+
+    
+
+    if (total == 1){  //Se hace el cambio de nivel
+         dibujar_meta();
+     }          
+
+}
+
+function dibujar_meta(){
+            if (x > canvas.width-300){
+                ctx.drawImage(img_meta,canvas.width-200, y_pista,100,210);
+                dibujar_carrito();
+
+                ctx.font="30px Times new roman"
+                ctx.fillStyle="dark blue"
+                ctx.fillText("Felicidades, \n pasaste el nivel: "+(nivel+1),canvas.width/2-100,150);
+
+                //clearInterval(IntervaloJuego);
+                nivel+=1;
+                intervalo_carrito -= 2;
+                contador = 0;
+            }
+
+}
+
 
 function dibujar_pista(){
     ctx.fillStyle = "#BBB5B1";
@@ -213,3 +374,5 @@ function dibujar_divisiones(){
         ctx.fillRect(0,y_divisiones, canvas.width, 10 );
     }
 }
+
+
